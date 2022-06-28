4f70260fdd71241ace5f4c888f1153f649228dbe
Poltorak Serguei <ps@z-wave.me>
2022-06-28 15:52:55 +0300

    Improved tests

--- testClient.py
+++ testClient.py
@@ -1,23 +1,26 @@
 # -*- coding: latin-1 -*-
 import time
+import threading
 import websocket
 
 def on_message(wsapp, message):
-    print(">>", message)
+    print("Received", message)
 
 def on_open(wsapp):
     f = open('testClient.txt', 'r')
     lines = f.readlines()
+    print("Opened connection")
+    t = threading.Thread(target=thread_function, args=(lines,))
+    t.start()
+
+def thread_function(lines):
+    print("Start sending")
     for line in lines:
-        time.sleep(0.1)
-        print(">>")
+        time.sleep(0.01)
         message = line.encode("utf-8")
-        print("<<", message)
-        print("<<", message)
+        print("Sending", message)
         wsapp.send(message)
     
 wsapp = websocket.WebSocketApp("ws://localhost:9090", on_message=on_message, on_open=on_open)
 
 wsapp.run_forever()
-
-
